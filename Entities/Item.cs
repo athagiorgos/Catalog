e9e10ca8a73c2d10b309d54d09245846e72f0e25
athagiorgos <athan711@gmail.com>
2022-02-04 12:21:11 +0200

    Project setup

---
+++ Entities/Item.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; init; } // init after the creation you can no longer modify this
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
