e10bfe5c8e21e1bf2ceadb53bfd97bc78f7fbb0a
Giorgos Athanasopoulos <athan711@gmail.com>
2022-02-07 19:19:36 +0200

    Initialized to docker

--- Program.cs
+++ Program.cs
@@ -45,9 +45,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
